b2a21bf0f55838530e26748ae864846ffa2c723f
Anonymous9564 <122253446+Subhadip956425@users.noreply.github.com>
2024-05-30 19:21:35 +0530

    Add files via upload

---
+++ Kruskal_Algorithm.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Graph {
+    int V, E;
+    struct Edge* edge;
+};
+
+struct Graph* createGraph(int V, int E) {
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->E = E;
+    graph->edge = (struct Edge*) malloc(graph->E * sizeof(struct Edge));
+    return graph;
+}
+
+int find(int parent[], int i) {
+    while (parent[i] != i) {
+        i = parent[i];
+    }
+    return i;
+}
+
+void Union(int parent[], int x, int y) {
+    int xset = find(parent, x);
+    int yset = find(parent, y);
+    parent[xset] = yset;
+}
+
+int compare(const void* a, const void* b) {
+    struct Edge* a1 = (struct Edge*) a;
+    struct Edge* b1 = (struct Edge*) b;
+    return a1->weight > b1->weight;
+}
+
+void KruskalMST(struct Graph* graph) {
+    int V = graph->V;
+    struct Edge result[V];  
+    int e = 0;  
+    int i = 0;  
+    qsort(graph->edge, graph->E, sizeof(graph->edge[0]), compare);
+
+    int* parent = (int*) malloc(V * sizeof(int));
+
+    for (int v = 0; v < V; ++v)
+        parent[v] = v;
+
+    while (e < V - 1 && i < graph->E) {
+        struct Edge next_edge = graph->edge[i++];
+
+        int x = find(parent, next_edge.src);
+        int y = find(parent, next_edge.dest);
+
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(parent, x, y);
+        }
+    }
+
+    printf("Following are the edges in the constructed MST\n");
+    for (i = 0; i < e; ++i)
+        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
+
+    free(parent);
+}
+
+int main() {
+    int V, E;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &V);
+    printf("Enter the number of edges: ");
+    scanf("%d", &E);
+
+    struct Graph* graph = createGraph(V, E);
+
+    printf("Enter the source, destination, and weight of each edge:\n");
+    for (int i = 0; i < E; i++) {
+        printf("Edge %d: ", i + 1);
+        scanf("%d %d %d", &graph->edge[i].src, &graph->edge[i].dest, &graph->edge[i].weight);
+    }
+
+    KruskalMST(graph);
+
+    free(graph->edge);
+    free(graph);
+
+    return 0;
+}
